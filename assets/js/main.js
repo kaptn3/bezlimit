bb8a71144aa4f28bff351b75464ec43da4ba4f89
victoria <kapitonenko.vick@ya.ru>
2019-11-22 01:35:44 +0300

    feat: next slide

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,11 +15,21 @@ const cloneSlides = (box, items) => {
   box.append(createSlide(0, items));
 }
 
+const nextSlide = (box, items) => {
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].classList.contains('carousel__item_active')) active = i;
+  }
+  items[active].classList.remove('carousel__item_active');
+  active++;
+  items[active].classList.add('carousel__item_active');
+  box.style.transform = `translateX(${-25.5 - 35 * (active)}rem)`;
+}
+
 const initCarousel = () => {
   const box = document.querySelector('.carousel__box');
   const items = document.querySelectorAll('.carousel__item');
   cloneSlides(box, items);
-  //const interval = setInterval(nextSlide, 4000);
+  const interval = setInterval(nextSlide, 2000, box, items);
 }
 
 
